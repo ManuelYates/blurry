f13ff0ad0284683c8e07bf3ddaca0d1f9d9fde5d
MY <53792937+ManuelYates@users.noreply.github.com>
2020-01-28 02:26:25 +0100

    sql_ini created
    sql_ini creates the needed DB's and create a user "admin" in the Db so you can directly access the system and dont have to register every time.

--- intern/sql_ini.php
+++ intern/sql_ini.php
@@ -1,19 +1,55 @@
-<!DOCTYPE html>
 <?php
+$pdo = new PDO('mysql:host=localhost;dbname=test', 'root', '');
 
-function initializeSQL(){
-  echo "Hello";
-}
+$statement = 'DROP DATABASE blurry';
+$pdo->exec($statement);
+echo "Die alte DB wurde gelöscht<br>";
+
+$statement = 'CREATE DATABASE blurry';
+$pdo->exec($statement);
+echo "Die neue DB wurde erstellt<br>";
+
+$pdo = null;
+$pdo = new PDO('mysql:host=localhost;dbname=blurry', 'root', '');
+
+$statement = "CREATE TABLE users (
+id INT NOT NULL AUTO_INCREMENT,
+email VARCHAR(255),
+passwort varchar(255),
+vorname varchar(255),
+nachname varchar(255),
+created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+updated_at timestamp on update CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ,
+user_role int(1),
+profile_img_path varchar(255),
+PRIMARY KEY (`id`), UNIQUE (`email`)
+) ENGINE = InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;";
+$pdo->exec($statement);
+echo "Die neue Tabelle users wurde erstellt<br>";
+
+$admin_email = 'admin@blurry.de';
+$admin_passwort ='root';
+$admin_vorname = 'Admin';
+$admin_nachname = 'Admin';
+$admin_user_role = 3;
+$passwort_hash = password_hash($admin_passwort, PASSWORD_DEFAULT);
+$statement = $pdo->prepare("INSERT INTO users (email, passwort, vorname, nachname, user_role) VALUES (:email, :passwort, :vorname, :nachname, :user_role)");
+$result = $statement->execute(array('email' => $admin_email, 'passwort' => $passwort_hash, 'vorname' => $admin_vorname, 'nachname'=> $admin_nachname, 'user_role' => $admin_user_role));
+echo "Das Administratorkonto wurde erstellt<br>";
 
 
+$statement = 'CREATE TABLE img_list (
+img_id INT NOT NULL AUTO_INCREMENT,
+img_path VARCHAR(255),
+img_name VARCHAR(255),
+img_creator VARCHAR(255),
+uploaded_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+PRIMARY KEY (`img_id`)
+) ENGINE = InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;';
+$pdo->exec($statement);
+echo "Die neue Tabelle img_list wurde erstellt<br>";
+
+
+echo "Die benötigten Tabellen wurden erstellt!<br>";
+echo "<br> Hier gelangen Sie zurück zur Hauptseite";*/
  ?>
-
-<html lang="de" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
-    <button onClick="initializeSQL" type="button" name="button">Test</button>
-  </body>
-</html>
---
+++ intern/sql_ini_ui.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+
+<html lang="de" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <form action="sql_ini.php" method="post" enctype="multipart/form-data">
+      <table .class="table_upload">
+        <tr>
+          <th>Sind Sie sicher, dass Sie die Tabellen initalisieren möchten?</th>
+        </tr>
+        <tr>
+          <td>Stellen Sie sicher, dass MySQL und Apache im Hintergrund aktiv sind und Sie die richtigen Benutzerdaten eingegeben haben!</td>
+        </tr>
+        <br>
+      </table>
+      <input type="submit" value="Hochladen">
+  </body>
+</html>
