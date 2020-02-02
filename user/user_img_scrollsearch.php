aba6a9a75469e7a073c5a6e9e278bf989b9f79a8
MY <53792937+ManuelYates@users.noreply.github.com>
2020-02-02 06:20:46 +0100

    Blur-Background added

--- user/user_img_scrollsearch.php
+++ user/user_img_scrollsearch.php
@@ -34,10 +34,11 @@
 
   if ($result-> num_rows > 0) {
       while ($row = $result-> fetch_assoc()) {
-          echo "<div class='image_main'><img src=".$row['img_path']."><br>
+          echo "<div class= img_main><div class='image_inner'><img src=".$row['img_path']."><br>
       <table class='img_info'>
       <tr><th>Name:</th><th>Ersteller:</th><th>Upload-Datum:</th></tr>
-      <tr><td>".$row['img_name']."</td><td>".$row['img_creator']."</td><td>".$row['uploaded_at']."</td></tr></table></div>";
+      <tr><td>".$row['img_name']."</td><td>".$row['img_creator']."</td><td>".$row['uploaded_at']."</td></tr></table></div>
+      <div class='image_outer' style='background-image: url(".$row['img_path'].");background-size: cover;'></div></div>";
       }
   } else {
       echo "<h1>Kein Eintrag gefunden</h1>";
