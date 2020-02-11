18a446ab98fbc472229c4a8b0c22124c05426342
MY <53792937+ManuelYates@users.noreply.github.com>
2020-02-11 15:10:23 +0100

    Added Verslog table to sql_ini.php

--- intern/sql_ini.php
+++ intern/sql_ini.php
@@ -39,7 +39,6 @@ $statement = $pdo->prepare("INSERT INTO users (email, passwort, vorname, nachnam
 $result = $statement->execute(array('email' => $admin_email, 'passwort' => $passwort_hash, 'vorname' => $admin_vorname, 'nachname'=> $admin_nachname, 'user_role' => $admin_user_role));
 echo "Das Administratorkonto wurde erstellt<br>";
 
-
 $statement = 'CREATE TABLE img_list (
 img_id INT NOT NULL AUTO_INCREMENT,
 img_path VARCHAR(255),
@@ -52,6 +51,16 @@ PRIMARY KEY (`img_id`)
 $pdo->exec($statement);
 echo "Die neue Tabelle img_list wurde erstellt<br>";
 
+$statement = 'CREATE TABLE verslog (
+verslog_id INT NOT NULL AUTO_INCREMENT,
+verslog_title VARCHAR(255),
+verslog_text VARCHAR(255),
+verslog_num INT NOT NULL,
+uploaded_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+PRIMARY KEY (`verslog_id`)
+) ENGINE = InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;';
+$pdo->exec($statement);
+echo "Die neue Tabelle verslog wurde erstellt<br>";
 
 echo "Die benötigten Tabellen wurden erstellt!<br>";
 echo "<br> <a href=".$link_user_img_scrollsearch.">Hier gelangen Sie zurück zur Hauptseite</a>";
