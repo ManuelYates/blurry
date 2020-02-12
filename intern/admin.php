5217b75f6feffa6b13d9d11ad244aced5e488259
MY <53792937+ManuelYates@users.noreply.github.com>
2020-02-12 18:23:33 +0100

    Added som JS to admin.php

--- intern/admin.php
+++ intern/admin.php
@@ -30,42 +30,45 @@ if (isset($_GET['versupload'])) {
   <?php print $html_head ?>
 </head>
 
+<script>
+  function fshowVerslog() {
+    var vVerslog;
+    vVerslog = '<form action="?versupload=1" method="post" enctype="multipart/form-data">'
+    vVerslog += '<table>';
+    vVerslog += '<tr><th>Version Titel</th><td><input name="verslog_title" type="text"></td></tr>';
+    vVerslog += '<tr><th>Version</th><td><input name="verslog_num" type="int"></td></tr>';
+    vVerslog += '<tr><th>Versionsbeschreibung</th><td><input name="verslog_text" type="text"></td></tr>';
+    vVerslog += '</table><input type="submit" value="Hochladen"></form>';
+    document.getElementById('idresult').innerHTML = vVerslog;
+  }
+
+  function fshowImageScroll() {
+    var vImageScroll;
+    vImageScroll = '<div id="content_img"><?php echo ImageScroll() ?>';
+    document.getElementById('idresult').innerHTML = vImageScroll;
+  }
+</script>
+
 <body>
   <?php print $html_header ?>
 
   <table>
     <tr>
-      <td><button>Versionslog hinzufügen</button></td>
-      <td><button>Bilder löschen</button></td>
+      <td><button onclick="fshowVerslog();">Versionslog hinzufügen</button></td>
+      <td><button onclick="fshowImageScroll();">Bilder löschen</button></td>
       <td><a href="<?php echo $link_sql_ini ?>"><button>DB Reset</button></a></td>
     </tr>
   </table>
 
   <div id="verslog_added">
-    <form action="?versupload=1" method="post" enctype="multipart/form-data">
-      <table>
 
-        <tr>
-          <th>Version's Titel</th>
-          <td><input name="verslog_title" type="text"></td>
-        </tr>
-        <tr>
-          <th>Version</th>
-          <td><input name="verslog_num" type="int"></td>
-        </tr>
-        <tr>
-          <th>Versionsbeschreibung</th>
-          <td><input name="verslog_text" type="text"></td>
-        </tr>
-      </table>
-      <input type="submit" value="Hochladen">
-    </form>
-    </div>
+  </div>
 
-   <div id="content_img">
-      <?php echo ImageScroll()?>
-    
-    <?php print $html_footer ?>
+
+
+
+  <div id="idresult"></div>
+  <?php print $html_footer ?>
 </body>
 
 </html>
